b7aae46ad4e15ca71b60e12946515307df4590b1
flplira <47536635+flplira@users.noreply.github.com>
2020-09-10 23:13:57 -0300

    Update Exercicies.py

--- Exercicies.py
+++ Exercicies.py
@@ -215,3 +215,246 @@ target.write("\n")
 print ("And finally, we close it. ")
 target.close()
 
+
+
+#ext24.py
+
+print("Let’ s practice everything. ")
+print("You\’d need to know \’bout escapes with \\ that do \n newlines and \t tabs. ")
+
+poem = '''
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+'''
+
+print("--------------")
+print(poem)
+print("--------------")
+five = 10 - 2 + 3 - 6
+
+print ("This should be five: {}".format(five))
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return int(jelly_beans), int(jars), int(crates)
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+print("With a starting point of: {}".format(start_point))
+print("We’d have {} beans, {} jars, and {} crates. ".format(beans,jars,crates))
+
+start_point = start_point / 10
+print("We can also do that this way: ")
+print(secret_formula(start_point))
+values = (secret_formula(start_point))
+#print("We’d have %s beans, %s jars, and %s crates."% (secret_formula(start_point)))
+print("We’d have {} beans, {} jars, and {} crates.".format(values[0],values[1],values[2]))
+
+
+#ext25.py
+
+def break_words(stuff):
+    '''This function will break up words for us.'''
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    '''Sorts the words.'''
+    return sorted(words)
+
+def print_first_word(words):
+    '''Prints the first word after popping it off.'''
+    word = words.pop(0)
+    print (word)
+
+def print_last_word(words):
+    '''Prints the last word after popping it off.'''
+    word = words.pop(-1 )
+    print (word)
+
+def sort_sentence(sentence):
+    '''Takes in a full sentence and returns the sorted words.'''
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    '''Prints the first and last words of the sentence.'''
+    words = break_words(sentence)
+    print_first_word (words)
+    print_last_word (words)
+
+def print_first_and_last_sorted(sentence):
+    '''Sorts the words then prints the first and last one.'''
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+    
+#ext25-1.py
+
+import ext25
+
+sentence = "All good things come to those who wait."
+
+words = ext25.break_words(sentence)
+print(words)
+
+sorted_words = ext25.sort_words(words)
+print(sorted_words)
+
+ext25.print_first_word(words)
+ext25.print_last_word(words)
+print(words)
+
+ext25.print_first_word(sorted_words)
+ext25.print_last_word(sorted_words)
+print(sorted_words)
+
+sorted_words = ext25.sort_sentence(sentence)
+print(sorted_words)
+
+ext25.print_first_and_last(sentence)
+ext25.print_first_and_last_sorted(sentence)
+
+
+#ext26.py
+
+import ext25
+
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print (word)
+
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print (word)
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+
+print("Let's practice everything.")
+print("You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.")
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explantion
+\n\t\twhere there is none.
+"""
+
+
+print("--------------")
+print (poem)
+print ("--------------")
+
+five = 10 + 2 - 3 - 5
+print ("This should be five: {}".format(five))
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return int(jelly_beans), int(jars), int(crates)
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+print("With a starting point of: {}".format(start_point))
+print("We'd have {} jeans, {} jars, and {} crates.".format(beans,jars,crates))
+
+start_point = start_point / 10
+
+print("We can also do that this way:")
+
+print("We'd have {} beans, {} jars, and {} crabapples.".format(secret_formula(start_point)[0],secret_formula(start_point)[1],secret_formula(start_point)[2]))
+
+sentence = "All god\tthings come to those who weight."
+
+words = ext25.break_words(sentence)
+sorted_words = ext25.sort_words(words)
+
+print_first_word(words)
+print_last_word(words)
+print_first_word(sorted_words)
+print_last_word(sorted_words)
+sorted_words = ext25.sort_sentence(sentence)
+print(sorted_words)
+
+print_first_and_last(sentence)
+
+print_first_and_last_sorted(sentence)
+
+
+
+#ext27.py
+
+True and 1 == 1
+False and 0 != 0
+
+True and True
+False and True
+1 == 1 and 2 == 1
+"test" == "test"
+1 == 1 or 2 != 1
+True or 1 == 1
+"test" == "testing"
+1 != 0 and 2 == 1
+"test" != "testing"
+"test" == 1
+not (True and False)
+not (1 == 1 and 0 != 1)
+not (10 == 1 or 1000 == 1000)
+not (1 != 10 or 3 == 4)
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+1 == 1 and (not ("testing" == 1 or 1 == 0))
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
